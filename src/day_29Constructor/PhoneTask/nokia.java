2d04d082ef59dfe69a047afcd11b92e39778529e
copacinschi90 <>
2023-04-26 21:32:26 -0400

    States

---
+++ src/day_29Constructor/PhoneTask/nokia.java
@@ -0,0 +1,11 @@
+package day_29Constructor.PhoneTask;
+
+public class nokia extends Phone{
+
+        public void selfDefense(){
+            System.out.println(getBrand()+" "+getModel()+" can be used for self defense");
+        }
+
+    }
+
+
